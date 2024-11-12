eef2d8e0713b268537dfd37ae66bb26def2a0a3b
jsusmachaca <falcorgd@gmail.com>
2024-11-11 23:54:53 -0500

    feat: integrates router and middleware

---
+++ server/api/middleware/auth_middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jsusmachaca/fileserver/internal/util"
+	"github.com/jsusmachaca/go-router/pkg/response"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		if len(authorization) < 1 {
+			response.JsonErrorFromString(w, "Token not provided", http.StatusUnauthorized)
+			return
+		}
+		tokenString := authorization[len("Bearer "):]
+
+		fmt.Printf("header %s, token %s\n", authorization, tokenString)
+
+		_, err := util.ValidateToken(tokenString)
+		if err != nil {
+			log.Println(err.Error())
+			response.JsonErrorFromString(w, "Invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/jsusmachaca/fileserver/api/handler"
+	"github.com/jsusmachaca/fileserver/api/middleware"
+	"github.com/jsusmachaca/go-router/pkg/router"
 )
 
-func main() {
-	mux := http.NewServeMux()
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("\033[31mNot .env file found. Using system variables\033[0m")
+	}
+}
 
-	mux.HandleFunc("GET /fs/", handler.GetFiles)
-	mux.HandleFunc("PUT /fs/upload", handler.UploadFiles)
-	mux.HandleFunc("GET /list/", handler.ListDir)
+func main() {
+	PATH_DIR := os.Getenv("PATH_DIR")
+	PORT := os.Getenv("PORT")
+
+	route := router.NewRouter()
+
+	getFiles := &handler.GetFiles{PathDir: PATH_DIR}
+	uploadFiles := &handler.UploadFiles{PathDir: PATH_DIR}
+	listDir := &handler.ListDir{PathDir: PATH_DIR}
+
+	route.Get("/fs/", middleware.AuthMiddleware, getFiles)
+	route.Put("/fs/upload/", middleware.AuthMiddleware, uploadFiles)
+	route.Get("/list/", middleware.AuthMiddleware, listDir)
 
 	server := http.Server{
-		Addr:    ":8082",
-		Handler: mux,
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: route.ServeMux,
 	}
-	fmt.Println("Server listen on port 8082")
+	fmt.Printf("Server listen on port %s\n", PORT)
 	server.ListenAndServe()
 }
