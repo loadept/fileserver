66ae1f084f7ea5d9d7bc26f7e1f01ca38cbc6e10
jsusmachaca <falcorgd@gmail.com>
2024-11-13 00:50:39 -0500

    feat: create database migration

--- server/internal/database/database.go
+++ server/internal/database/database.go
@@ -26,3 +26,28 @@ func GetConnection() (*sql.DB, error) {
 	log.Println("Connect to SQLITE database")
 	return db, nil
 }
+
+func Migrate(db *sql.DB) error {
+	const create string = `
+	CREATE TABLE IF NOT EXISTS user (
+		id VARCHAR(36) PRIMARY KEY,
+		username VARCHAR(50) UNIQUE,
+		first_name VARCHAR(100),
+		email VARCHAR(100),
+		password VARCHAR(100),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`
+
+	res, err := db.Exec(create)
+	if err != nil {
+		return err
+	}
+
+	_, err = res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	log.Println("Migration completed")
+	return nil
+}
