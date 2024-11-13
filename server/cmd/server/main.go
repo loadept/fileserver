a898648db011984f9a37746aba187a2572d3fc20
jsusmachaca <falcorgd@gmail.com>
2024-11-12 21:26:56 -0500

    feat: initializes connection and debug dependencies

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,12 +10,21 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/jsusmachaca/fileserver/api/handler"
 	"github.com/jsusmachaca/fileserver/api/middleware"
+	"github.com/jsusmachaca/fileserver/internal/database"
 	"github.com/jsusmachaca/go-router/pkg/router"
 )
+
+var db *sql.DB
 
 func init() {
 	if err := godotenv.Load(".env"); err != nil {
 		log.Println("\033[31mNot .env file found. Using system variables\033[0m")
+	}
+
+	var err error
+	db, err = database.GetConnection()
+	if err != nil {
+		log.Fatalln("Error to connect database")
 	}
 }
 
@@ -27,10 +37,14 @@ func main() {
 	getFiles := &handler.GetFiles{PathDir: PATH_DIR}
 	uploadFiles := &handler.UploadFiles{PathDir: PATH_DIR}
 	listDir := &handler.ListDir{PathDir: PATH_DIR}
+	login := &handler.Login{DB: db}
+	register := &handler.Register{DB: db}
 
 	route.Get("/fs/", middleware.AuthMiddleware, getFiles)
 	route.Put("/fs/upload/", middleware.AuthMiddleware, uploadFiles)
 	route.Get("/list/", middleware.AuthMiddleware, listDir)
+	route.Post("/login", nil, login)
+	route.Post("/register", nil, register)
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%s", PORT),
--- server/go.mod
+++ server/go.mod
@@ -6,4 +6,8 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.5.1
 	github.com/jsusmachaca/go-router v1.0.1
+	github.com/mattn/go-sqlite3 v1.14.24
+	golang.org/x/crypto v0.29.0
 )
+
+require github.com/google/uuid v1.6.0
