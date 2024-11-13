f654b57d0e7a2c15cee228f223e2d2bb9f873ad3
jsusmachaca <falcorgd@gmail.com>
2024-11-13 12:31:53 -0500

    refactor: change template method render

--- server/api/handler/ssr.go
+++ server/api/handler/ssr.go
@@ -3,19 +3,12 @@ package handler
 import (
 	"html/template"
 	"net/http"
-
-	"github.com/jsusmachaca/go-router/pkg/response"
 )
 
 type Index struct{}
-type Gallery struct{}
 
 func (h *Index) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/template/index.html")
-	if err != nil {
-		response.JsonErrorFromString(w, "Error to render template", http.StatusInternalServerError)
-		return
-	}
+	tmpl := template.Must(template.ParseFiles("web/template/index.html"))
 
 	tmpl.Execute(w, nil)
 }
